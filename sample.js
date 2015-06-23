47cc3885b98f67912a714bf9e19278ba55229043
Taj <sayyadtaj786@gmail.com>
2015-06-23 16:28:57 +0530

    port and address not showing

--- sample.js
+++ sample.js
@@ -1,1 +1,13 @@
-console.log("welcome");
+var express=require('express');
+var app=express();
+
+app.get('/',function(req,res){
+  res.send("hello world");
+
+});
+
+var server=app.listen(3001,function(){
+  var host=server.address().address;
+  var port=server.address().port;
+  console.log("example app listening at http://%s%s",host,port);
+});
